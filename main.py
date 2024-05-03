0ec937f06bad6024d9491bbab1ccec68152b7605
notasimp440 <james89082@students.sbcsc.k12.in.us>
2024-05-02 23:06:49 -0400

    back ground image imported

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import turtle as trtl
+import tkinter as tk
 cry_man = 'kms.jpg'
 rammy = 'rammy.jpg'
+bkg = 'background.png'
+trtl.bgpic = bkg
 trtl.shape = rammy
 trtl.shape = cry_man
 
 trtl.forward(100)
+
+
+
+trtl.getscreen()
+trtl.screensize(360, 240)
