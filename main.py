861afa67d97fd6ad538a4ed32fe961f0977310a8
James Wilson <james89082@students.sbcsc.k12.in.us>
2024-05-03 09:18:00 -0400

    we have rammy now

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle as tur
+tur.setup(1500,1500)
 wn = tur.Screen()
 
 #our pics
@@ -7,11 +8,24 @@ rammy = 'rammy.gif'
 
 #all the mumbo jumbo
 wn.addshape(rammy)
+wn.addshape(cry_man)
+
+def player_right():
+    player.forward(20)
 
 #plyaer
 player = tur.Turtle()
 player.shape(rammy)
-
-
-
+player.shapesize(1,1,1)
 player.forward(100)
+
+
+while True:
+    tur.listen
+
+    tur.onkey(player_right, 'Right')
+
+
+
+
+    wn.mainloop()
